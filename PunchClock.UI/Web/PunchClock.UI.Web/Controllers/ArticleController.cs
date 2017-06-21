2a4fdc6a81253b8a04377ea1f9e4c4420c427af9
Harsha Bopuri <hbopuri@hotmail.com>
2017-06-21 15:58:31 +0530

    Get Company ID by current user session

--- PunchClock.UI/Web/PunchClock.UI.Web/Controllers/ArticleController.cs
+++ PunchClock.UI/Web/PunchClock.UI.Web/Controllers/ArticleController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace PunchClock.UI.Web.Controllers
 {
+    [Authorize]
     public class ArticleController : Controller
     {
         // GET: CMS
